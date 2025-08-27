dcd646fc2e45e7fb25719c7d24479d07564021f8
Viniciusgigante19 <viniciusgigante935@gmail.com>
2025-08-27 19:39:44 -0300

    feature:se clicar em um li ele pode desfazer o eveno

--- unifaat-frontend-tf-aula02-css-seletores/public/js/app.js
+++ unifaat-frontend-tf-aula02-css-seletores/public/js/app.js
@@ -2,10 +2,14 @@ const ulElement = document.querySelector(".content ul.demo");
 const liElementCollection = ulElement.querySelectorAll("li");
 
 liElementCollection.forEach((li) => {
-    li.addEventListener("click", (event) => {
-        ulElement.querySelectorAll("li").forEach((el) => {
-            el.classList.remove("selected");
-        });
-        event.currentTarget.classList.add("selected");
+    li.addEventListener('click', (event) => {
+        if (li.classList.contains('selected')) {
+            li.classList.remove("selected");
+        } else {
+            ulElement.querySelectorAll("li").forEach((el) => {
+                el.classList.remove("selected");
+            });
+            event.currentTarget.classList.add("selected");
+        }
     });
 });
